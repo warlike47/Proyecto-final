9829e4a6bb90f3bf5fab8afa5b97e7e94c738757
warlike47 <47108381+warlike47@users.noreply.github.com>
2019-02-05 17:52:39 -0500

    Add files via upload
    Ya se puede registrar el usuario desde consola

--- ProyectoHTML/Backend proyecto final segundo semestre.cpp
+++ ProyectoHTML/Backend proyecto final segundo semestre.cpp
@@ -3,101 +3,143 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-
-void Escribir(int suma);
+#include <string.h>
 
 using namespace std;
 
-/*
-void Lectura(){
-     ifstream file;
-     
-     file.open("C:\Users\Cisco\Desktop\Pagesq\no c.html", ios::in);
-     
-     if(file.fail()){
-         cout<<"No se pudo abrir el archivo"<<endl;
-         exit(1);                
-     }
-     
-}*/
+struct DatosUsuario{
+       char Name[100];      //Contiene el nombre de la persona
+       long long int ID;      //Contiene la cedula de la persona
+       char User[100];       //Contiene el nombre de usuario con el que se vá a ingresar
+       char password[100];        //Contiene la contraseña con la que el usuario va a ingresar
+};
 
-int PideDatos(){
-    int num1 = 4;
-    int num2 = 6;
-    
-    return num1+num2;
-}
+//--------------------------------------------------//
+//ESTAS SON LAS FIRMAS DE LAS FUNCIONES DEL PROGRAMA//
+//--------------------------------------------------//
 
+void Escribir();
+void IngresarDatos(DatosUsuario &datosDelUsuario);
+void RegistroUsuario(DatosUsuario &datosDelUsuario, bool &estaRegistrado);
+
+//---------------------------//
+//AQUI TERMINAN LAS FIRMAS XD//
+//---------------------------//
+
+//FUNCION PRINCIPAL
 int main()
 {
-    int suma;
-    suma = PideDatos();
-    Escribir(suma);
+    struct DatosUsuario datosDelUsuario;        //Variable que referencia la estructura con los datos del usuario.
+    bool estaRegistrado = false;        //Contiene la verificacion si el usuario está registrado o no.
+    
+    RegistroUsuario(datosDelUsuario, estaRegistrado);        //Registra al usuario si no lo está, si lo está, pide los datos para loguearse
+    
+    
+    Escribir();      //Realiza la maquetacion de la vista.
     //Lectura();
     system("PAUSE");
     return 0;
 }
 
-void Escribir(int suma){
-    ofstream archivo;
-    ifstream file;
-    int xd = suma;
+//Registra al usuario si no lo está, si lo está, pide los datos para loguearse
+void RegistroUsuario(DatosUsuario &datosDelUsuario, bool &estaRegistrado){
+     
+     //Valida si el usuario está registrado
+     if(!estaRegistrado){
+          //ALMACENA TODOS LOS DATOS NECESARIOS PARA EL REGISTRO EN LA ESTRUCTURA DatosUsuario
+          cout<<"Ingrese su nombre completo= ";
+          fflush(stdin);
+          gets(datosDelUsuario.Name);
+          cout<<"\n"<<endl;
+          cout<<"Ingrese su numero de cedula= ";
+          cin>>datosDelUsuario.ID;
+          cout<<"\n"<<endl;
+          cout<<"Ingrese el usuario que desea= ";
+          fflush(stdin);
+          gets(datosDelUsuario.User);
+          cout<<"\n"<<endl;
+          cout<<"Ingrese la contraseña que desea= ";
+          fflush(stdin);
+          gets(datosDelUsuario.password);
+          
+          system("cls");
+          //Con esta parte confirmamos siquedan guardados los datos.
+          cout<<datosDelUsuario.Name<<endl;
+          cout<<datosDelUsuario.ID<<endl;
+          cout<<datosDelUsuario.User<<endl;
+          cout<<datosDelUsuario.password<<endl;
+          //Si pilla, si quedan guardados xD.
+          
+          estaRegistrado = true;
+     }else{
+          IngresarDatos(datosDelUsuario);         //INGRESA LOS DATOS SI YA ESTÁ REGISTRADO.
+     }
+}
+
+//Funcion que pide los datos de usuario para poder ingresar.
+void IngresarDatos(DatosUsuario &datosDelUsuario){
+     
+}
+
+//Funcion que realiza la maquetacion de la vista.
+void Escribir(){
+    ofstream archivoHTML;     //Contiene el archivo HTML de la vista
     
-    archivo.open("Frontend proyecto final segundo semestre.html", ios::out);
-    if(archivo.fail()){
+    archivoHTML.open("Frontend proyecto final segundo semestre.html", ios::out);     //Abre el archivo HTML
+    if(archivoHTML.fail()){
         cout<<"No se pudo leer el archivo, error fatal";
         exit(1);
     }
     
-    archivo<<"<!doctype html>";
+    archivoHTML<<"<!doctype html>";
     
-    archivo<<"<html lang='es'>";
+    archivoHTML<<"<html lang='es'>";
     
-    archivo<<"<head>";
+    archivoHTML<<"<head>";
     
-	archivo<<"<title>Proyecto final programacion 2 Andrés Manuel Prieto Álvarez & Cristian Andrés Grajales Perez</title>";
-    archivo<<"<meta charset='utf-8''>";
-    archivo<<"<link rel='shortcut icon' type='/imagex-icon' href='icon.png'>";
-    archivo<<"<link rel='stylesheet' href='jQuery/Plugins_jQuery/fancybox/v_3.5.1/jquery_FancyBox.3.5.1.css'>";
-    archivo<<"<link rel='stylesheet' type='text/css' href='frontend para proyecto final del segundo semestre.css'>";
-    archivo<<"<script src='jQuery/jquery-3.3.1.min.js' type='text/javascript'></script>";
-    archivo<<"<script src='jQuery/Plugins_jQuery/jquery-easing_plugin_v1.3.js' type='text/javascript'></script>";
+	archivoHTML<<"<title>Proyecto final programacion 2 Andrés Manuel Prieto Álvarez & Cristian Andrés Grajales Perez</title>";
+    archivoHTML<<"<meta charset='utf-8''>";
+    archivoHTML<<"<link rel='shortcut icon' type='/imagex-icon' href='icon.png'>";
+    archivoHTML<<"<link rel='stylesheet' href='jQuery/Plugins_jQuery/fancybox/v_3.5.1/jquery_FancyBox.3.5.1.css'>";
+    archivoHTML<<"<link rel='stylesheet' type='text/css' href='frontend para proyecto final del segundo semestre.css'>";
+    archivoHTML<<"<script src='jQuery/jquery-3.3.1.min.js' type='text/javascript'></script>";
+    archivoHTML<<"<script src='jQuery/Plugins_jQuery/jquery-easing_plugin_v1.3.js' type='text/javascript'></script>";
     //archivo<<"<script src='jQuery/Plugins_jQuery/fancybox/v_3.5.1/jquery_FancyBox.3.5.1.js type='text/javascript></script>";
-    archivo<<"<script>$(document).ready(function() {$('#banner').hide();$('#fecha_llegada').click(function(e){$('#banner').fadeIn(2000);e.preventDefault();return false;})});</script>";
-	archivo<<"</head>";
+    archivoHTML<<"<script>$(document).ready(function() {$('#banner').hide();$('#fecha_llegada').click(function(e){$('#banner').fadeIn(2000);e.preventDefault();return false;})});</script>";
+	archivoHTML<<"</head>";
     
-    archivo<<"<body>";
+    archivoHTML<<"<body>";
     
-	archivo<<"<header>";
+	archivoHTML<<"<header>";
     
-    archivo<<"<div id='search_div'>";
-    archivo<<"<input type='text' placeholder='Buscar' name='Buscar' id='search'>";
-    archivo<<"<input type='button' value='Buscar' id='button_buscar'>";
-    archivo<<"</div>";
+    archivoHTML<<"<div id='search_div'>";
+    archivoHTML<<"<input type='text' placeholder='Buscar' name='Buscar' id='search'>";
+    archivoHTML<<"<input type='button' value='Buscar' id='button_buscar'>";
+    archivoHTML<<"</div>";
     
-	archivo<<"<div id='dates'>";
-    archivo<<"<input type='button' value='Fecha de llegada'>";
-    archivo<<"<input type='button' value='Fecha de salida'>";
-    archivo<<"<input type='button' value='Menu'>";
-    archivo<<"</div";
+	archivoHTML<<"<div id='dates'>";
+    archivoHTML<<"<input type='button' value='Fecha de llegada'>";
+    archivoHTML<<"<input type='button' value='Fecha de salida'>";
+    archivoHTML<<"<input type='button' value='Menu'>";
+    archivoHTML<<"</div";
     
-	archivo<<"</header>";
+	archivoHTML<<"</header>";
     
-    archivo<<"<section class='main'>";
+    archivoHTML<<"<section class='main'>";
     
     for(int i=0;i<5;i++){
-    	archivo<<"<div class='galeria'>";
-	    archivo<<"<img src='cancun.jpg' alt='imagen'>";
-	    archivo<<"<div>";
-	    archivo<<"<h2 class='refence'><a href='https://goo.gl/maps/hxJcrHy1dYt' class='reference' target='_blank'>Hotel Occidental Costa de Cancun</a></h2>";
-	    archivo<<"</div>";
-	    archivo<<"</div>";
+    	archivoHTML<<"<div class='galeria'>";
+	    archivoHTML<<"<img src='cancun.jpg' alt='imagen'>";
+	    archivoHTML<<"<div>";
+	    archivoHTML<<"<h2 class='refence'><a href='https://goo.gl/maps/hxJcrHy1dYt' class='reference' target='_blank'>Hotel Occidental Costa de Cancun</a></h2>";
+	    archivoHTML<<"</div>";
+	    archivoHTML<<"</div>";
 	}
     
-    archivo<<"</section>";
+    archivoHTML<<"</section>";
     
-	archivo<<"</body>";
-    archivo<<"</html>";
+	archivoHTML<<"</body>";
+    archivoHTML<<"</html>";
     
-    archivo.close();
+    archivoHTML.close();
 }

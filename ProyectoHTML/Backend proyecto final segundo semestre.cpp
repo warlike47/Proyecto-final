48e181b30d939560ed3bd0765c58785c935d635f
warlike47 <47108381+warlike47@users.noreply.github.com>
2019-02-07 17:48:34 -0500

    Add files via upload
    Ya sirve para iniciar sesión

--- ProyectoHTML/Backend proyecto final segundo semestre.cpp
+++ ProyectoHTML/Backend proyecto final segundo semestre.cpp
@@ -46,19 +46,22 @@ struct DatosAdministrador{
 //ESTAS SON LAS FIRMAS DE LAS FUNCIONES DEL PROGRAMA//
 //--------------------------------------------------//
 
-void Escribir();
+void Escribir(short op);
 void IngresarDatos(DatosUsuario &datosDelUsuario);
 void RegistroUsuario(DatosUsuario &datosDelUsuario, bool &estaRegistrado, DatosAdministrador &datosDelAdministrador);
 void RegistroAdministrador(DatosAdministrador &datosDelAdministrador, bool &estaRegistrado, DatosUsuario &datosDelUsuario);
 void MenuPrincipal(DatosUsuario &datosDelUsuario, bool &estaRegistrado, DatosAdministrador &datosDelAdministrador);
 void DefineTipoUsuario(DatosUsuario &datosDelUsuario, bool &estaRegistrado, DatosAdministrador &datosDelAdministrador);
+void Loguearse(DatosUsuario &datosDelUsuario, bool &estaRegistrado, DatosAdministrador &datosDelAdministrador);
+void LoguearseComoAdministrador(DatosAdministrador &datosDelAdministrador, bool &estaRegistrado);
+void LoguearseComoUsuario(DatosUsuario &datosDelUsuario, bool &estaRegistrado);
 
 
 
 void MuestraDatosUsuarios(DatosUsuario &datosDelUsuario, bool &estaRegistrado);
 void MuestraDatosAdmins(DatosAdministrador &datosDelAdministrador, bool &estaRegistrado);
 //---------------------------//
-//AQUI TERMINAN LAS FIRMAS XD//
+//AQUI TERMINAN LAS FIRMAS   //
 //---------------------------//
 
 //FUNCION PRINCIPAL
@@ -69,7 +72,7 @@ int main()
     bool estaRegistrado = false;        //Contiene la verificacion si el usuario está registrado o no.
     system("color 4B");
     
-    Escribir();      //Realiza la maquetacion de la vista.
+    Escribir(1);      //Realiza la maquetacion de la vista.
     MenuPrincipal(datosDelUsuario, estaRegistrado, datosDelAdministrador);
     //RegistroUsuario(datosDelUsuario, estaRegistrado);        //Registra al usuario si no lo está, si lo está, pide los datos para loguearse
     //RegistroUsuario(datosDelUsuario, estaRegistrado);
@@ -83,42 +86,135 @@ int main()
 void MenuPrincipal(DatosUsuario &datosDelUsuario, bool &estaRegistrado, DatosAdministrador &datosDelAdministrador){
      
      int op = 1;       //Es la opcion que va a tomar el usuario
-     datosDelUsuario.cantUsuarios = 0;
-     datosDelAdministrador.cantAdministradores = 0;
+     datosDelUsuario.cantUsuarios = 0;       //Dice que al principio del programa hay 0 usuarios
+     datosDelAdministrador.cantAdministradores = 0;        //Dice que al principio del programa hay 0 administradores
      
      //Se ejecuta si op es diferente de 0
      while(op != 0){
           system("cls");
           //Muestra las opciones que se pueden ejecutar
           cout<<"Ingrese 1 para registrarte"<<endl;
-          cout<<"Ingrese 2 para buscar un hotel"<<endl;
+          cout<<"Ingrese 2 para loguearse"<<endl;
           cout<<"Ingrese 3 para mostrar todos los usuarios corrientes"<<endl;
           cout<<"Ingrese 4 para mostrar todos los Administradores"<<endl;
           cin>>op;       //Guarda el valor de la opcion que se ejecutará
           
           switch(op){
-               case 1:
-                   //Ejecuta el registro o logea al usuario dependiendo de si está registrado o no
+               case 1:{
+                   //Ejecuta el registro o loguea al usuario dependiendo de si está registrado o no
                    DefineTipoUsuario(datosDelUsuario, estaRegistrado, datosDelAdministrador);
                    system("pause");
                    break;
-               case 2:
-                   system("start Frontend_proyecto_final_segundo_semestre.html");
-                   system("start https://www.trivago.com.co/?aDateRange%5Barr%5D=2019-02-13");
+               }
+               case 2:{
+                    Loguearse(datosDelUsuario, estaRegistrado, datosDelAdministrador);
+                   //system("start Frontend_proyecto_final_segundo_semestre.html");
+                   //system("start https://www.trivago.com.co/?aDateRange%5Barr%5D=2019-02-13");
                    system("pause");
                    break;
-               case 3:
+               }
+               case 3:{
                     MuestraDatosUsuarios(datosDelUsuario, estaRegistrado);
                     system("pause");
                     break;
-               case 4:
+                }
+               case 4:{
                     MuestraDatosAdmins(datosDelAdministrador, estaRegistrado);
                     system("pause");
                     break;
+                }
           }
      }
 }
 
+//Pregunta si desea ingresar a la pagina como usuario o como administrador
+void Loguearse(DatosUsuario &datosDelUsuario, bool &estaRegistrado, DatosAdministrador &datosDelAdministrador){
+     int op = 1;
+     system("start Frontend_proyecto_final_segundo_semestre_pagina_de_presentacion.html"); 
+     
+     while(op != 0){
+          cout<<"Ingrese 1 para loguearse como usuario"<<endl;
+          cout<<"Ingrese 2 para loguearse como administrador"<<endl;
+          cin>>op; 
+          
+          switch(op){
+                 case 1:
+                      LoguearseComoUsuario(datosDelUsuario, estaRegistrado);
+                      break;
+                 case 2:
+                      LoguearseComoAdministrador(datosDelAdministrador, estaRegistrado);
+                      break;           
+          }        
+     }     
+}
+
+//Se ejecuta si se elige ingresar a la pagina como administrador
+void LoguearseComoAdministrador(DatosAdministrador &datosDelAdministrador, bool &estaRegistrado){
+     system("cls");
+     char NombreAdministrador[100];
+     cout<<"Ingrese su usuario de administrador= ";
+     fflush(stdin);
+     gets(NombreAdministrador);
+     cout<<endl;
+     char ContraseniaAdministrador[100];
+     cout<<"Ingrese su contraseña de administrador= ";
+     fflush(stdin);
+     gets(ContraseniaAdministrador);
+     cout<<endl;
+     
+     int cantLogIn = 0;
+     
+     for(int i = 0; i < datosDelAdministrador.cantAdministradores; i++){
+           if(NombreAdministrador == datosDelAdministrador.todosLosAdmin.UsersAdministradores[i]){
+                if(ContraseniaAdministrador == datosDelAdministrador.todosLosAdmin.ContrasAdministradores[i]){
+                      cout<<"Te has logueado exitosamente como administrador!"<<endl;
+                      cantLogIn++;
+                      system("start Frontend_proyecto_final_segundo_semestre_pagina_de_presentacion.html");        //Aqui va el inicio de la vista principal                    
+                }                       
+           }
+     }
+     
+     if(cantLogIn == 0){
+          cout<<"El usuario o la contraseña no corresponde con una cuenta"<<endl;             
+     }else{
+           cantLogIn = 0;      
+     }
+}    
+
+//Se ejecuta si se elije ingresar a la pagina como usuario corriente
+void LoguearseComoUsuario(DatosUsuario &datosDelUsuario, bool &estaRegistrado){
+     system("cls");
+     char NombreUsuario[100];
+     cout<<"Ingrese su usuario= ";
+     fflush(stdin);
+     gets(NombreUsuario);
+     cout<<endl;
+     char ContraseniaUsuario[100];
+     cout<<"Ingrese su contraseña= ";
+     fflush(stdin);
+     gets(ContraseniaUsuario);
+     cout<<endl;
+     
+     int cantLogIn = 0;
+     
+     for(int i = 0; i < datosDelUsuario.cantUsuarios; i++){
+           if(NombreUsuario == datosDelUsuario.todosLosUsuarios.UsersUsuarios[i]){
+                 if(ContraseniaUsuario == datosDelUsuario.todosLosUsuarios.ContrasUsuarios[i]){
+                        cout<<"Te has logueado exitosamente!"<<endl;
+                        cantLogIn++;
+                        system("start Frontend_proyecto_final_segundo_semestre_pagina_de_presentacion.html");          //Aqui va el inicio de la vista principal           
+                 }                 
+           }
+     }
+     
+     if(cantLogIn == 0){
+          cout<<"El usuario o la contraseña no corresponde con una cuenta"<<endl;
+     }else{
+          cantLogIn = 0;      
+     }
+}
+
+//Muestra todos los administradores de la pagina
 void MuestraDatosAdmins(DatosAdministrador &datosDelAdministrador, bool &estaRegistrado){
      for(int i = 0; i < datosDelAdministrador.cantAdministradores; i++){
           cout<<"ADMINISTRADOR "<<i+1<<endl;
@@ -132,6 +228,7 @@ void MuestraDatosAdmins(DatosAdministrador &datosDelAdministrador, bool &estaReg
 
 //FUNCION TEMPORAL QUE MUESTRA TODOS LOS USUARIOS DE LA BASE DE DATOS
 
+//Muestra todos los usuarios de la pagina
 void MuestraDatosUsuarios(DatosUsuario &datosDelUsuario, bool &estaRegistrado){
      
      for(int i = 0; i < datosDelUsuario.cantUsuarios; i++){
@@ -144,6 +241,7 @@ void MuestraDatosUsuarios(DatosUsuario &datosDelUsuario, bool &estaRegistrado){
      }
 }
 
+//Define si se quiere registrar como administrador o como usuario corriente
 void DefineTipoUsuario(DatosUsuario &datosDelUsuario, bool &estaRegistrado, DatosAdministrador &datosDelAdministrador){
      system("cls");
      int op = 1;
@@ -166,6 +264,7 @@ void DefineTipoUsuario(DatosUsuario &datosDelUsuario, bool &estaRegistrado, Dato
      }
 }
 
+//Se ejecuta si se elige registrar como administrador
 void RegistroAdministrador(DatosAdministrador &datosDelAdministrador, bool &estaRegistrado, DatosUsuario &datosDelUsuario){
      //Valida si el usuario está registrado
      if(!estaRegistrado){
@@ -188,6 +287,7 @@ void RegistroAdministrador(DatosAdministrador &datosDelAdministrador, bool &esta
           int cantRepeticionesCedula = 0;
           int cantRepeticionesUser = 0;
           
+          //Recorre todos los administradores y valida si puede crear la cuenta.
           for(int i = 0; i < datosDelAdministrador.cantAdministradores; i++){
                if(datosDelAdministrador.IDAdmin == datosDelAdministrador.todosLosAdmin.CedulasAdministradores[i]){
                    cantRepeticionesCedula++;                        
@@ -196,6 +296,7 @@ void RegistroAdministrador(DatosAdministrador &datosDelAdministrador, bool &esta
                    cantRepeticionesUser++;                    
                }
                for(int j = 0; j < datosDelUsuario.cantUsuarios; j++){
+                     cout<<"Segundo for de administrador"<<endl;
                      if(datosDelAdministrador.IDAdmin == datosDelUsuario.todosLosUsuarios.CedulasUsuarios[j]){
                          cantRepeticionesCedula++;                      
                      }
@@ -205,6 +306,7 @@ void RegistroAdministrador(DatosAdministrador &datosDelAdministrador, bool &esta
                }
           }
           
+          //Validaciones de entrada.
           if(cantRepeticionesCedula != 0){
               cantRepeticionesCedula = 0;
               cout<<"\nYa existe un usuario con esa cedula registrada\n"<<endl;
@@ -245,6 +347,7 @@ void RegistroAdministrador(DatosAdministrador &datosDelAdministrador, bool &esta
 }
 
 //Registra al usuario si no lo está, si lo está, pide los datos para loguearse
+//Se ejecuta si se desea registrar como usuario corriente.
 void RegistroUsuario(DatosUsuario &datosDelUsuario, bool &estaRegistrado, DatosAdministrador &datosDelAdministrador){
      
      //Valida si el usuario está registrado
@@ -268,6 +371,7 @@ void RegistroUsuario(DatosUsuario &datosDelUsuario, bool &estaRegistrado, DatosA
           int cantRepeticionesCedula = 0;
           int cantRepeticionesUser = 0;
           
+          //Recorre todos los usuarios y valida si se puede crear la cuenta.
           for(int i = 0; i < datosDelUsuario.cantUsuarios; i++){
                if(datosDelUsuario.ID == datosDelUsuario.todosLosUsuarios.CedulasUsuarios[i]){
                    cantRepeticionesCedula++;
@@ -276,6 +380,7 @@ void RegistroUsuario(DatosUsuario &datosDelUsuario, bool &estaRegistrado, DatosA
                    cantRepeticionesUser++;                    
                }
                for(int j = 0; j < datosDelAdministrador.cantAdministradores; j++){
+                     cout<<"Segundo for de usuario"<<endl;
                      if(datosDelUsuario.ID == datosDelAdministrador.todosLosAdmin.CedulasAdministradores[j]){
                          cantRepeticionesCedula++;                      
                      }
@@ -285,6 +390,7 @@ void RegistroUsuario(DatosUsuario &datosDelUsuario, bool &estaRegistrado, DatosA
                }
           }
           
+          //Validaciones de entrada
           if(cantRepeticionesCedula != 0){
               cantRepeticionesCedula = 0;
               cout<<"\nYa existe un usuario con esa cedula registrada\n"<<endl;
@@ -352,65 +458,113 @@ void IngresarDatos(DatosUsuario &datosDelUsuario){
      
 }
 
-//Funcion que realiza la maquetacion de la vista.
-void Escribir(){
-    ofstream archivoHTML;     //Contiene el archivo HTML de la vista
+
+//Funcion que realiza la maquetacion de la vista.     
+void Escribir(short op){
+    ofstream archivo;  //Contiene el archivo HTML de la vista
+    ifstream file;
     
-    archivoHTML.open("Frontend_proyecto_final_segundo_semestre.html", ios::out);     //Abre el archivo HTML
-    if(archivoHTML.fail()){
-        cout<<"No se pudo leer el archivo, error fatal";
-        exit(1);
-    }
-    
-    archivoHTML<<"<!doctype html>";
-    
-    archivoHTML<<"<html lang='es'>";
-    
-    archivoHTML<<"<head>";
-    
-	archivoHTML<<"<title>Proyecto final programacion 2 Andrés Manuel Prieto Álvarez & Cristian Andrés Grajales Perez</title>";
-    archivoHTML<<"<meta charset='utf-8''>";
-    archivoHTML<<"<link rel='shortcut icon' type='/imagex-icon' href='icon.png'>";
-    archivoHTML<<"<link rel='stylesheet' href='jQuery/Plugins_jQuery/fancybox/v_3.5.1/jquery_FancyBox.3.5.1.css'>";
-    archivoHTML<<"<link rel='stylesheet' type='text/css' href='frontend para proyecto final del segundo semestre.css'>";
-    archivoHTML<<"<script src='jQuery/jquery-3.3.1.min.js' type='text/javascript'></script>";
-    archivoHTML<<"<script src='jQuery/Plugins_jQuery/jquery-easing_plugin_v1.3.js' type='text/javascript'></script>";
-    //archivo<<"<script src='jQuery/Plugins_jQuery/fancybox/v_3.5.1/jquery_FancyBox.3.5.1.js type='text/javascript></script>";
-    archivoHTML<<"<script>$(document).ready(function() {$('#banner').hide();$('#fecha_llegada').click(function(e){$('#banner').fadeIn(2000);e.preventDefault();return false;})});</script>";
-	archivoHTML<<"</head>";
-    
-    archivoHTML<<"<body>";
-    
-	archivoHTML<<"<header>";
-    
-    archivoHTML<<"<div id='search_div'>";
-    archivoHTML<<"<input type='text' placeholder='Buscar' name='Buscar' id='search'>";
-    archivoHTML<<"<input type='button' value='Buscar' id='button_buscar'>";
-    archivoHTML<<"</div>";
-    
-	archivoHTML<<"<div id='dates'>";
-    archivoHTML<<"<input type='button' value='Fecha de llegada'>";
-    archivoHTML<<"<input type='button' value='Fecha de salida'>";
-    archivoHTML<<"<input type='button' value='Menu'>";
-    archivoHTML<<"</div";
-    
-	archivoHTML<<"</header>";
-    
-    archivoHTML<<"<section class='main'>";
-    
-    for(int i=0;i<5;i++){
-    	archivoHTML<<"<div class='galeria'>";
-	    archivoHTML<<"<img src='cancun.jpg' alt='imagen'>";
-	    archivoHTML<<"<div>";
-	    archivoHTML<<"<h2 class='refence'><a href='https://goo.gl/maps/hxJcrHy1dYt' class='reference' target='_blank'>Hotel Occidental Costa de Cancun</a></h2>";
-	    archivoHTML<<"</div>";
-	    archivoHTML<<"</div>";
+    if(op == 1){
+    	archivo.open("Frontend_proyecto_final_segundo_semestre_pagina_de_presentacion.html", ios::out);  //Abre el archivo HTML
+	    if(archivo.fail()){
+	        cout<<"No se pudo leer el archivo, error fatal";
+	        exit(1);
+	    }
+	    
+	    archivo<<"<!doctype html>";
+	    
+	    archivo<<"<html lang='es'>";
+	    
+	    archivo<<"<head>";
+	    archivo<<"<title>Proyecto final programacion 2 Andres Manuel Prieto Alvarez & Cristian Andres Grajales Perez</title>";
+	    archivo<<"<meta charset='utf-8'>";
+	    archivo<<"<link rel='shortcut icon type='/imagex-icon href='../Imagenes/icon.png'>";
+	    archivo<<"<link rel='stylesheet' type='text/css' href='../CSS/frontend de presentacion.css'>";
+	    archivo<<"<script src='../jQuery/jquery-3.3.1.min.js' type='text/javascript'></script>";
+	    archivo<<"<script>";
+	    
+	    archivo<<"$(document).ready(function(){$('#banner').hide();$('#boton').click(function(e){$('#banner').fadeIn(2000);$('#boton').addClass('whenClick');e.preventDefault();return false;})});";
+	    
+		archivo<<"var begin=0; function strart(op) {if (op){begin=1;}}function beginner(){return begin;}";
+	    
+		archivo<<"</script>";
+		
+		archivo<<"</head>";
+		
+		archivo<<"<body>";
+		
+		archivo<<"<section class='main'>";
+		archivo<<"<img src='../Imagenes/presentacion.png' alt='avión' id='boton'>";
+		archivo<<"<div id='banner'>";
+		archivo<<"<form action='ingreso' id='ingreso'>";
+		archivo<<"<label for='correo'>Nombre de usuario</label>";
+		archivo<<"<input type='text' placeholder='Ingrese su usuario'>";
+		archivo<<"<br><br>";
+		archivo<<"<label for='contrasena'>Ingrese su contrasena</label>";
+		archivo<<"<input type='text' placeholder='Ingrese su contrasena'>";
+		archivo<<"</form>";
+		archivo<<"<h2><a href='#' onclick='strart(1)'>Cootravels S.A.S</a></h2>";
+		archivo<<"</div>";
+		archivo<<"</section>";
+		archivo<<"</body>";
+		archivo<<"</html>";
+	}else{
+		if(op == 2){
+			archivo.open("Frontend proyecto final segundo semestre pagina principal.html", ios::out);
+		    if(archivo.fail()){
+		        cout<<"No se pudo leer el archivo, error fatal";
+		        exit(1);
+		    }
+		    
+		    archivo<<"<!doctype html>";
+		    
+		    archivo<<"<html lang='es'>";
+		    
+		    archivo<<"<head>";
+		    
+			archivo<<"<title>Proyecto final programacion 2 Andrés Manuel Prieto Álvarez & Cristian Andrés Grajales Perez</title>";
+		    archivo<<"<meta charset='utf-8''>";
+		    archivo<<"<link rel='shortcut icon' type='/imagex-icon' href='../Imagenes/icon.png'>";
+		    archivo<<"<link rel='stylesheet' type='text/css' href='../CSS/frontend para proyecto final del segundo semestre.css'>";
+		    archivo<<"<script src='../jQuery/jquery-3.3.1.min.js' type='text/javascript'></script>";
+		    
+		    archivo<<"<script>$(document).ready(function() {$('#banner').hide();$('#fecha_llegada').click(function(e){$('#banner').fadeIn(2000);e.preventDefault();return false;})});</script>";
+			
+			archivo<<"</head>";
+		    
+		    archivo<<"<body>";
+		    
+			archivo<<"<header>";
+		    
+		    archivo<<"<div id='search_div'>";
+		    archivo<<"<input type='text' placeholder='Buscar' name='Buscar' id='search'>";
+		    archivo<<"<input type='button' value='Buscar' id='button_buscar'>";
+		    archivo<<"</div>";
+		    
+			archivo<<"<div id='dates'>";
+		    archivo<<"<input type='button' value='Fecha de llegada'>";
+		    archivo<<"<input type='button' value='Fecha de salida'>";
+		    archivo<<"<input type='button' value='Menu'>";
+		    archivo<<"</div>";
+		    
+			archivo<<"</header>";
+		    
+		    archivo<<"<section class='main'>";
+		    
+		    for(int i=0;i<5;i++){
+				archivo<<"<div class='galeria'>";
+				archivo<<"<img src='../Imagenes/cancun.jpg' alt='imagen'>";
+				archivo<<"<div>";
+				archivo<<"<h2 class='refence'><a href='https://goo.gl/maps/hxJcrHy1dYt' class='reference' target='_blank'>Hotel Occidental Costa de Cancun</a></h2>";
+				archivo<<"</div>";
+				archivo<<"</div>";
+			}
+		    
+			archivo<<"</section>";
+		    
+			archivo<<"</body>";
+			archivo<<"</html>";
+	    	archivo.close();
+		}	
 	}
-    
-    archivoHTML<<"</section>";
-    
-	archivoHTML<<"</body>";
-    archivoHTML<<"</html>";
-    
-    archivoHTML.close();
 }
